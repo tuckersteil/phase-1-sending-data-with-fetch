1959877478d866c9ab8a014ea0db9f47e9e7558b
Tucker Steil <tuckersteil@gmail.com>
2022-04-19 14:50:09 -0400

    commit tree a04b5650b40dc236598760b32cd67c72840fca7b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+function submitData(name, email){
+    const confgObj = {
+        method: "POST",
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": name,
+            "email": email
+        })
+    };
+    return fetch("http://localhost:3000/users", confgObj)
+    .then(res => res.json())
+    .then(body => addNewContact(body.id))
+
+    .catch(resposnse => document.body.append(resposnse))
+}
+
+function addNewContact(newId){
+    document.getElementsByTagName("body")[0].innerHTML = newId; 
+}
